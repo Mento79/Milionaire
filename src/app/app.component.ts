1abc112220ae085fe441fc1bd0a7da984d1d1121
Mento79 <bodyomar79@gmail.com>
2022-01-25 20:57:13 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Milion';
+  currentQuestion="  اسشيصخبشىخىصيخهي خصشهت يخهشصت خه صتخشهصتيخشهصتي خصشيى خصش تىخشصهتي خشهصت خهتشص خهتيصش خهتصش خه تص"
+}
