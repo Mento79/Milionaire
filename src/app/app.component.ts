1bb4019f53448adcb081a484088cd4140d3239fa
Mento79 <bodyomar79@gmail.com>
2022-01-26 09:32:56 +0200

    finsh ques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { timer } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Milion';
-  currentQuestion="  اسشيصخبشىخىصيخهي خصشهت يخهشصت خه صتخشهصتيخشهصتي خصشيى خصش تىخشصهتي خشهصت خهتشص خهتيصش خهتصش خه تص"
+  currentQuestion=" مين اكل الجبنة"
+  currentAnswerA="  ا. محسن"
+  currentAnswerB="  ب. محسنة"
+  currentAnswerC="  ج. محسنين"
+  currentAnswerD="  د. محسنون"
+
+  answered=false
+
+  handle(x:string){
+
+    if (!this.answered){
+      this.answered=true
+      document.getElementById("da")!.className="container"
+      document.getElementById("db")!.className="container"
+      document.getElementById("dc")!.className="container"
+      document.getElementById("dd")!.className="container"
+  
+      timer(1000).subscribe(y => { if (x=="c"){
+        (<HTMLImageElement>document.getElementById(x))!.src="assets/ans2Cor.png"
+      }
+      else{
+        (<HTMLImageElement>document.getElementById(x))!.src="assets/ans2X.png"
+  
+      } })
+  
+    }
+    
+  }
 }
