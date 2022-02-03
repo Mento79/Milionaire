6333abfbe60a2050de27286abf6ef48504298e6a
Mento79 <bodyomar79@gmail.com>
2022-02-03 17:52:11 +0200

    nnice

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -4,41 +4,73 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import com.google.gson.Gson;
+import javafx.scene.control.TextArea;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 //import org.json.simple.JSONArray;
 //import org.json.simple.JSONObject;
 
 public class HelloController {
+  HashMap<String, Object> Ques = new HashMap<String, Object>();
+  private final Gson gson = new Gson();
+  int num=0;
+  File myObj;
+
+
+
   @FXML
   private Label welcomeText;
   @FXML
   private ComboBox<String> selector;
   @FXML
   private ChoiceBox<String> selector1;
+  @FXML
+  private TextArea question;
+  @FXML
+  private TextArea a1;
+  @FXML
+  private TextArea a2;
+  @FXML
+  private TextArea a3;
+  @FXML
+  private TextArea a4;
 
   @FXML
-  protected void onHelloButtonClick() {
-//
-//    JSONObject employeeDetails = new JSONObject();
-//    employeeDetails.put("firstName", "Lokesh");
-//    employeeDetails.put("lastName", "Gupta");
-//    employeeDetails.put("website", "howtodoinjava.com");
-//    try (FileWriter file = new FileWriter("employees.json")) {
-//      //We can write any JSONArray or JSONObject instance to the file
-//      file.write(employeeDetails.toJSONString());
-//      file.flush();
-//    }
-//    catch (IOException e) {
-//      e.printStackTrace();
-//    }
+  protected void onHelloButtonClick() throws IOException {
+    Question x=new Question(question.getText(),a1.getText(),a2.getText(),a3.getText(),a4.getText(),Integer.parseInt(selector1.getValue()));
+    Ques.put(String.valueOf(num), x);
+    num++;
+    FileWriter myWriter = new FileWriter(myObj);
+    myWriter.write(gson.toJson(Ques));
+    myWriter.close();
+    System.out.println(gson.toJson(Ques));
   }
 
   @FXML
   protected void initialize() {
     selector.getItems().addAll("apple","asd","asdas");
     selector1.getItems().addAll("1","2","3");
+    try {
+      myObj = new File("que.json");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch ( FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    JSONObject jsonObject = new JSONObject(JString);
+    JSONArray loadIds = jsonObject.getJSONArray("IDs");
 
   }
 }
---
+++ demo/src/main/java/com/example/demo/Question.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Question {
+  public String question;
+  public String A1;
+  public String A2;
+  public String A3;
+  public String A4;
+  public int level;
+
+  public Question(String question,String A1,String A2,String A3,String A4,int level){
+    this.question=question;
+    this.A1=A1;
+    this.A2=A2;
+    this.A3=A3;
+    this.A4=A4;
+    this.level=level;
+  }
+}
