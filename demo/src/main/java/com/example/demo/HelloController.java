c3a4fd7b1b802628e88e9d2c182ef2a45b52f1a1
Mento79 <bodyomar79@gmail.com>
2022-02-03 18:14:54 +0200

    progress

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -11,8 +11,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 //import org.json.simple.JSONArray;
 //import org.json.simple.JSONObject;
@@ -22,6 +26,7 @@ public class HelloController {
   private final Gson gson = new Gson();
   int num=0;
   File myObj;
+  ArrayList<Question>QuesArr = new ArrayList<Question>();
 
 
 
@@ -45,32 +50,37 @@ public class HelloController {
   @FXML
   protected void onHelloButtonClick() throws IOException {
     Question x=new Question(question.getText(),a1.getText(),a2.getText(),a3.getText(),a4.getText(),Integer.parseInt(selector1.getValue()));
-    Ques.put(String.valueOf(num), x);
-    num++;
+    QuesArr.add(x);
     FileWriter myWriter = new FileWriter(myObj);
-    myWriter.write(gson.toJson(Ques));
+    myWriter.write(gson.toJson(QuesArr));
     myWriter.close();
-    System.out.println(gson.toJson(Ques));
+    System.out.println(gson.toJson(QuesArr));
   }
 
   @FXML
   protected void initialize() {
-    selector.getItems().addAll("apple","asd","asdas");
     selector1.getItems().addAll("1","2","3");
+    String data="";
     try {
       myObj = new File("que.json");
       Scanner myReader = new Scanner(myObj);
       while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
+        data = myReader.nextLine();
       }
       myReader.close();
+      JSONArray arr = new JSONArray(data);
+      System.out.println(arr);
+      for(int i = 0; i < arr.length(); i++){
+        JSONObject curr = new JSONObject(arr.get(i).toString());
+        System.out.println(curr);
+        System.out.println(arr.get(i));
+        selector.getItems().add(curr.getString("question"));
+      }
+
     } catch ( FileNotFoundException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
-    JSONObject jsonObject = new JSONObject(JString);
-    JSONArray loadIds = jsonObject.getJSONArray("IDs");
 
   }
 }
